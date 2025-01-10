913c9bcf71ff4a04eca76733d5dddd04ffb0ce1a
nokanowayi <962245699@qq.com>
2025-01-10 21:25:34 +0800

    基础

--- Assets/Script/Gun/Gun.cs
+++ Assets/Script/Gun/Gun.cs
@@ -2,17 +2,43 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Gun : MonoBehaviour
 {
+    public GameObject enemy;
     public GameObject towel;
     public Rigidbody2D rb;
     public Animator animator;
     public Vector2 direction; 
     public int speed;
+    public ObjectPool<GameObject> pool;
+    
+
     private void Update()
     {
-        //transform.position = towel.transform.position; 
+        CountDirection();
+        Move();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+         pool.Release(gameObject);   
+    }
+    
+
+    public void CountDirection()
+    {
+        float rangeX = 0;
+        float rangeY = 0;
+        rangeX = -(transform.position.x-enemy.transform.position.x);
+        rangeY = -(transform.position.x-enemy.transform.position.y);
+        direction = new Vector2(rangeX,rangeY).normalized;
+        //transform.rotation = new Quaternion(0,0,Mathf.Asin(direction.y),0);
+    }
+
+    public void Move()
+    {
         rb.velocity = direction.normalized*speed;
     }
 }
--- Assets/Script/Towel/Towel1.cs
+++ Assets/Script/Towel/Towel1.cs
@@ -1,26 +1,63 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Profiling.LowLevel.Unsafe;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Towel1 : Towel
 {
+    public ObjectPool<GameObject> gunPool;
     public GameObject gun;
     public GameObject enemy;
     public float attackRange;
-    public Vector2 direction;
     public float waitTime;
     public float waitTimeCounter;
     public bool isAttacking;
+
+    private void Awake()
+    {
+        gunPool = new ObjectPool<GameObject>(CreatPool,GetPool,ReleasePool,DestroyPool,true,100,1000);
+    }
+
+    public void DestroyPool(GameObject obj)
+    {
+        Destroy(obj);
+    }
+
+    public void GetPool(GameObject obj)
+    {
+        obj.transform.position = transform.position;
+        obj.SetActive(true);
+    }
+
+    public void ReleasePool(GameObject obj)
+    {
+        obj.SetActive(false);   
+    }
+
+    public GameObject CreatPool()
+    {
+        var obj = Instantiate(gun,transform.position,Quaternion.identity);
+        obj.GetComponent<Gun>().pool = gunPool;
+        obj.GetComponent<Gun>().enemy = this.enemy;
+        return obj;
+    }
+
     private void Update()
     {
         if (enemy !=null)
         {
-            if (attackRange<CountRange()&!isAttacking)
-            {isAttacking = true;
+            if (attackRange>CountRange())
+            {
              Attack();
-             WaitTimeCounter();
+             isAttacking = true;
             }
+        }
+
+        if (isAttacking)
+        {
+            WaitTimeCounter();
         }
     }
 
@@ -29,10 +66,9 @@ public class Towel1 : Towel
         float rangeX = 0;
         float rangeY = 0;
         float realRange = 0;
-        rangeX = Mathf.Abs(transform.position.x-enemy.transform.position.x);
-        rangeY = Mathf.Abs(transform.position.x-enemy.transform.position.y);
-        direction = new Vector2(rangeX,rangeY).normalized;
-        realRange = rangeX * rangeX+rangeY * rangeY;
+        rangeX = transform.position.x-enemy.transform.position.x;
+        rangeY = transform.position.x-enemy.transform.position.y;
+        realRange = Mathf.Sqrt( rangeX * rangeX+rangeY * rangeY);
         return realRange;
     }
 
@@ -48,7 +84,10 @@ public class Towel1 : Towel
         
     public override void Attack()
     {
-        Instantiate(gun,transform.position,Quaternion.identity);
+        if (!isAttacking)
+        {
+            gunPool.Get();
+        }
     }
 
     public override void Hurt()
